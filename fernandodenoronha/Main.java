c3b543e7cef33b8902b5be4972f1411a1b11f74d
Marquin <141364618+garciaog@users.noreply.github.com>
2023-10-27 16:19:39 -0300

    Update Main.java

--- fernandodenoronha/Main.java
+++ fernandodenoronha/Main.java
@@ -5,15 +5,12 @@ public class Main {
     public static void main(String[] args) { 
       
         String[] times = {
-                "América Mineiro", "Athletico-PR", "Atlético Mineiro", "Bahia", "Botafogo",
-                "Corinthians", "Coritiba", "Cruzeiro", "Cuiabá", "Flamengo", 
-                "Fluminense", "Fortaleza", "Goiás", "Grêmio", "Internacional", 
-                "Ponte-Preta", "Red Bull Bragantino", "Santos", "São Paulo", "Vasco" 
+            "América Mineiro", "Athletico-PR", "Atlético Mineiro", "Bahia", "Botafogo",
+            "Corinthians", "Coritiba", "Cruzeiro", "Cuiabá", "Flamengo", 
+            "Fluminense", "Fortaleza", "Goiás", "Grêmio", "Internacional", 
+            "Ponte-Preta", "Red Bull Bragantino", "Santos", "São Paulo", "Vasco" 
         }; 
         int[] pontuacoes = new int[times.length]; 
-                                                  
-
-
         int[] Vitórias = new int[times.length];
 
         Scanner scanner = new Scanner(System.in); 
@@ -33,44 +30,47 @@ public class Main {
                     scanner.close(); 
                     return; 
                 case 1: 
+                    ordenarPorVitoriasEPontuacao(times, pontuacoes, Vitórias);
                     exibirTabela(times, pontuacoes, Vitórias); 
                     break; 
                 case 2: 
                     editarPontuacao(times, pontuacoes, Vitórias, scanner); 
                     break; 
-                    case 3:
+                case 3:
                     editarVitórias(times, pontuacoes, Vitórias, scanner);
                     break;
                 default:
-
-                System.out.println("Opção inválida. Tente novamente."); 
+                    System.out.println("Opção inválida. Tente novamente."); 
                     break; 
             } 
         } 
     } 
-      
 
     public static void exibirTabela(String[] times, int[] pontuacoes, int[] Vitórias) { 
-        System.out.println("\nPosição | Time | Pontuação | Vitórias"); 
+        System.out.println("\nPosição / Time / Vitórias / Pontuação"); 
         for (int i = 0; i < times.length; i++) {
-            System.out.println((i + 1) + " | " + times[i] + " | " + pontuacoes[i] + " | " + Vitórias[i]); 
-                                                                                  
+            System.out.print((i + 1) + " / ");
+            if (i < 4) {
+                // Coloring the top 4 teams
+                System.out.print("\u001B[31m"); // Red
+            } else {
+                System.out.print("\u001B[0m"); // Reset color
+            }
+            System.out.print(times[i] + " / ");
+            System.out.print(Vitórias[i] + " / ");
+            System.out.print(pontuacoes[i]);
+            System.out.println("\u001B[0m"); // Reset color
         } 
     } 
 
-     
     public static void exibirTimes(String[] times, int[] pontuacoes, int[] Vitórias) { 
         for (int i = 0; i < times.length; i++) { 
-            System.out.println((i + 1) + " - " + times[i] + " | Pontuação: " + pontuacoes[i] + " | Vitórias:" + Vitórias[i]); 
-                                                                                            
+            System.out.println((i + 1) + " - " + times[i] + " | Vitórias: " + Vitórias[i] + " | Pontuação: " + pontuacoes[i]); 
         } 
     } 
 
-    
     public static void editarPontuacao(String[] times, int[] pontuacoes, int[] Vitórias, Scanner scanner) { 
-                                                                                           
         while (true) { 
-            
             System.out.println("\nTimes disponíveis para edição:"); 
             exibirTimes(times, pontuacoes, Vitórias); 
             System.out.println("Escolha o número do time para editar a pontuação (ou 0 para voltar):"); 
@@ -94,23 +94,23 @@ public class Main {
 
             scanner.nextLine(); 
         } 
-        ordenarPorPontuacaoEVitórias(times, pontuacoes, Vitórias ); 
+        ordenarPorVitoriasEPontuacao(times, pontuacoes, Vitórias); 
     } 
 
     public static void editarVitórias(String[] times, int[] pontuacoes, int[] Vitórias, Scanner scanner) {
         while(true) {
             System.out.println("\nTimes disponíveis para edição:");
             exibirTimes(times, pontuacoes, Vitórias);
-            System.out.println("\nEscolha o número do time para editar os Vitórias(ou 0 para voltar):");
+            System.out.println("\nEscolha o número do time para editar as Vitórias (ou 0 para voltar):");
             int posicao = scanner.nextInt();
-            if (posicao==0) {
+            if (posicao == 0) {
                 break;
             }
             if (posicao >= 1 && posicao <= times.length) {
-                System.out.println("Digite o novo número de Vitórias do" + times[posicao - 1] + ":");
-                int novosCartoes = scanner.nextInt();
-                if (novosCartoes >= 0) {
-                    Vitórias[posicao - 1] = novosCartoes;
+                System.out.println("Digite o novo número de Vitórias do " + times[posicao - 1] + ":");
+                int novasVitorias = scanner.nextInt();
+                if (novasVitorias >= 0) {
+                    Vitórias[posicao - 1] = novasVitorias;
                 } else {
                     System.out.println("O número de Vitórias não pode ser negativo.");
                 }
@@ -119,28 +119,26 @@ public class Main {
             }
             scanner.nextLine();
         }
+        ordenarPorVitoriasEPontuacao(times, pontuacoes, Vitórias);
     }
 
-   
-    public static void ordenarPorPontuacaoEVitórias(String[] times, int[] pontuacoes, int[] Vitórias) {
-        for (int i = 0; i < pontuacoes.length - 1; i++) {
-            for (int j = i + 1; j < pontuacoes.length; j++) {
-                
-                if (pontuacoes[i] < pontuacoes[j] || (pontuacoes[i] == pontuacoes[j] && Vitórias[i] > Vitórias[j])) {
-           
+    public static void ordenarPorVitoriasEPontuacao(String[] times, int[] pontuacoes, int[] Vitórias) {
+        for (int i = 0; i < Vitórias.length - 1; i++) {
+            for (int j = i + 1; j < Vitórias.length; j++) {
+                if (Vitórias[i] < Vitórias[j] || (Vitórias[i] == Vitórias[j] && pontuacoes[i] < pontuacoes[j])) {
                     int tempPontuacao = pontuacoes[i];
                     pontuacoes[i] = pontuacoes[j];
                     pontuacoes[j] = tempPontuacao;
-                    
-                    int tempCartoes = Vitórias[i];
+
+                    int tempVitorias = Vitórias[i];
                     Vitórias[i] = Vitórias[j];
-                    Vitórias[i] = tempCartoes;
-                
+                    Vitórias[j] = tempVitorias;
+
                     String tempTime = times[i];
                     times[i] = times[j];
                     times[j] = tempTime;
-              }
-              }
-              }
-              }
-              }
+                }
+            }
+        }
+    }
+}
